d34de114fc5bb58f447e5d98bc6931b6809cdd7e
0himu1 <0himu1@gmail.com>
2023-01-04 01:36:07 +0600

    set-up with eslint prettier done, also some styling

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Rettingpage from './Components/Rettingpage';
+import FinishPage from './Components/FinishPage';
+
+function App() {
+  return (
+    <div className="container">
+      <Rettingpage />
+      <FinishPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Numberbox.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style/Numberbox.css';
+
+export default function Numberbox() {
+  return (
+    <div className="numbers">
+      <div className="number">1</div>
+      <div className="number">2</div>
+      <div className="number">3</div>
+      <div className="number">4</div>
+      <div className="number">5</div>
+    </div>
+  );
+}
